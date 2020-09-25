ed3e5b51db63372167120c6491082969b04fb362
Evan <evan-brass@protonmail.com>
2020-09-25 11:54:41 -0700

    _

--- client/src/crypto.rs
+++ client/src/crypto.rs
@@ -1,11 +1,29 @@
-use std::convert::From;
+use std::{
+	convert::From,
+	ops::{ Deref, DerefMut }
+};
 use serde::{
 	Serialize,
 	Deserialize,
 	ser::Serializer,
 	de::Deserializer
 };
-use p256::ecdsa::signature::Signature as _;
+
+use p256::{
+	EncodedPoint,
+	Scalar, NonZeroScalar, ProjectivePoint, AffinePoint,
+	ecdsa::{
+		signature::{
+			Signature as _
+		},
+	},
+	elliptic_curve::{
+		weierstrass::point::Decompress,
+		ff::PrimeField
+	}
+};
+use ecdsa;
+use anyhow::{Context, anyhow};
 
 // Simple new-type wrapper
 #[derive(Debug)]
@@ -25,6 +43,17 @@ impl<T> AsMut<T> for Wrapper<T> {
 		&mut self.0
 	}
 }
+impl<T> Deref for Wrapper<T> {
+	type Target = T;
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+impl<T> DerefMut for Wrapper<T> {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.0
+	}
+}
 impl<T> From<T> for Wrapper<T> {
 	fn from(input: T) -> Self {
 		Self (input)
@@ -35,6 +64,7 @@ impl<T: Clone> Clone for Wrapper<T> {
 		Wrapper (self.0.clone())
 	}
 }
+impl<T: Copy> Copy for Wrapper<T> {}
 impl<T: PartialEq> PartialEq for Wrapper<T> {
 	fn eq(&self, other: &Self) -> bool {
 		self.0.eq(&other.0)
@@ -97,7 +127,109 @@ impl<'de> Deserialize<'de> for Signature {
 }
 
 // TODO: Recoverable Signature:
-// pub type RecoverableSignature = Wrapper<(p256::ecdsa::Signature, bool)>;
+pub type RecoverableSignature = Wrapper<(p256::ecdsa::Signature, bool)>;
+
+impl From<RecoverableSignature> for Signature {
+	fn from(rs: RecoverableSignature) -> Signature {
+		Signature::from(rs.unwrap().0)
+	}
+}
+
+fn is_scalar_high(s: &Scalar) -> bool {
+	let modulus_shr1: Scalar = Scalar::from_repr([
+		127, 255, 255, 255, 128, 0, 0, 0,
+		127, 255, 255, 255, 255, 255, 255, 255,
+		222, 115, 125, 86, 211, 139, 207, 66,
+		121, 220, 229, 97, 126, 49, 146, 168,
+	].into()).unwrap();
+	s > &modulus_shr1
+}
+impl RecoverableSignature {
+	// STOLEN: This recoverable implementation was taken and edited from the recoverable implementation in the k256 crate.
+	#[allow(non_snake_case)]
+	pub fn try_sign_recoverable_prehashed(secret_scalar: &Scalar, ephemeral_scalar: NonZeroScalar, message_hash: &Scalar) -> Result<Self, anyhow::Error> {
+		let k_inverse: Scalar = Option::from(ephemeral_scalar.invert()).context("Failed to invert the ephemeral_scalar")?;
+		let k = ephemeral_scalar;
+	
+		// Compute 𝐑 = 𝑘×𝑮
+		let R = EncodedPoint::from((ProjectivePoint::generator() * k.as_ref()).to_affine());
+		let is_R_odd = bool::from(Scalar::from_bytes_reduced(R.y().unwrap()).is_odd());
+	
+		// Lift x-coordinate of 𝐑 (element of base field) into a serialized big
+		// integer, then reduce it into an element of the scalar field
+		let r = Scalar::from_bytes_reduced(&R.x());
+	
+		// Compute `s` as a signature over `r` and `z`.
+		let mut s: Scalar = k_inverse * (message_hash + &(r * secret_scalar));
+	
+		if s.is_zero().into() {
+			return Err(anyhow!("S cannot be zero."));
+		}
+
+		let is_s_high = is_scalar_high(&s);
+		if is_s_high {
+			s = -s;
+		}
+	
+		let signature = ecdsa::Signature::from_scalars(r, s).map_err(|_| anyhow!("Failed to create signature from r and s"))?;
+		
+		Ok(Self((signature, is_R_odd ^ is_s_high)))
+	}
+	#[allow(non_snake_case)]
+	pub fn recover(&self, message_hash: &Scalar) -> Result<PublicKey, anyhow::Error> {
+		let (ref sig, is_odd) = self.as_ref();
+		let r = sig.r();
+        let s = sig.s();
+        let z = message_hash;
+        let R = AffinePoint::decompress(&r.to_bytes(), (*is_odd as u8).into());
+
+        if R.is_some().into() {
+            let R = ProjectivePoint::from(R.unwrap());
+            let r_inv = r.invert().unwrap();
+            let u1 = -(r_inv * z);
+            let u2 = r_inv * *s;
+            let pk = ((ProjectivePoint::generator() * u1) + (R * u2)).to_affine();
+
+            // TODO(tarcieri): ensure the signature verifies?
+            Ok(PublicKey::from(EncodedPoint::from(pk)))
+        } else {
+            Err(anyhow!("Failed to decompress R point."))
+        }
+	}
+}
+
+// pub fn try_sign_recoverable_prehashed<K>(&self, ephemeral_scalar: &K, z: &Scalar) -> Result<(Signature, bool), Error>
+// 	where K: Borrow<Scalar> + Invert<Output = Scalar>	
+// {
+// 	let k_inverse = ephemeral_scalar.invert();
+// 	let k = ephemeral_scalar.borrow();
+
+// 	if k_inverse.is_none().into() || k.is_zero().into() {
+// 		return Err(Error::new());
+// 	}
+
+// 	let k_inverse = k_inverse.unwrap();
+
+// 	// Compute 𝐑 = 𝑘×𝑮
+// 	let R = (ProjectivePoint::generator() * k).to_affine();
+
+// 	// Lift x-coordinate of 𝐑 (element of base field) into a serialized big
+// 	// integer, then reduce it into an element of the scalar field
+// 	let r = Scalar::from_bytes_reduced(&R.x.to_bytes());
+
+// 	// Compute `s` as a signature over `r` and `z`.
+// 	let s = k_inverse * (z + &(r * self));
+
+// 	if s.is_zero().into() {
+// 		return Err(Error::new());
+// 	}
+
+// 	let mut signature = Signature::from_scalars(r, s)?;
+// 	let is_r_odd = bool::from(R.y.is_odd());
+// 	let is_s_high = signature.normalize_s()?;
+	
+// 	Ok((signature, is_r_odd ^ is_s_high))
+// }
 
 // use sha2::{ Digest, Sha256 };
 // use p256::{
